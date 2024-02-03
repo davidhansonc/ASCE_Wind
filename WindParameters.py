b32c90e79b5e023e84f11aa38105fbafd7fff9dc
davidhansonc <davidhanson.c@gmail.com>
2024-02-02 16:32:42 -0800

    update

--- WindParameters.py
+++ WindParameters.py
@@ -4,18 +4,17 @@ class WindParameters:
     def __init__(self, basic_wind_speed, exposure, eave_height, ground_elevation=0, directionality_factor=0.85, topographic_factor=1.0):
         self.exposure = exposure
         self.α, self.z_g = self._terrain_exposure()
-        self.z = eave_height #ft
         self.V = basic_wind_speed #mph
-        self.z_g = ground_elevation
         self.Kzt = topographic_factor
         self.Kd = directionality_factor #0.85 for building structures
         self.Ke = np.e**(-0.0000362 * self.z_g)
-        self.Kz = 2.01 * (z / z_g)**(2 / α)
+        self.Kz = self.velocity_pressure_coefficient(eave_height)
         self.q_z = 0.00256 * self.Kz * self.Kzt * self.Kd * self.Ke * self.V**2
 		  
 
-    def velocity_pressure_coefficient(self, height):
-
+    def velocity_pressure_coefficient(self, eave_height):
+        z = eave_height
+        Kz = 2.01 * (z / self.z_g)**(2 / self.α)
         return Kz
 			
 
